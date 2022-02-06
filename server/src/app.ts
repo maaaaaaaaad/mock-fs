2d51b3989750c26cf2bbea18aeea001a3e3ad7f7
DONG WOONG OH <integral5137@gmail.com>
2022-02-06 17:54:31 +0900

    set server

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+  res.send('welcome!')
+})
+
+app.listen('1234', () => {
+  console.log(`
+   Server listening on port: 1234
+`)
+})
