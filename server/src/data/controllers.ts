a869d4e297587868ed99992a03cd101157be2b99
DONG WOONG OH <integral5137@gmail.com>
2022-02-06 18:05:41 +0900

    set data controllers

---
+++ server/src/data/controllers.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import { resolve } from 'path'
+
+const basePath = resolve()
+
+const filename: any = {
+  message: resolve(basePath, 'src/data/message.json'),
+}
+
+export const readData = (target: any) => {
+  try {
+    return JSON.parse(fs.readFileSync(filename[target], 'utf-8'))
+  } catch (e: any) {
+    console.log(e.message)
+  }
+}
+
+export const writeData = (target: any, data: any) => {
+  try {
+    return fs.writeFileSync(filename[target], JSON.stringify(data))
+  } catch (e: any) {
+    console.log(e.message)
+  }
+}
